ca052f546bcdf7038eb7fad6fd75cb60fee59f2c
timf34 <farrelti@tcd.ie>
2023-04-04 20:47:37 +0100

    made player bounding boxes tighter

--- config.py
+++ config.py
@@ -5,7 +5,12 @@ from typing import Dict, Tuple
 class Config:
     def __init__(self):
         self.image_dir: str = "data/images"
-        self.blue_player_bbox: Dict[str, int] = {"x1": 660, "y1": 505, "x2": 760, "y2": 635}
-        self.white_player_bbox: Dict[str, int] = {"x1": 715, "y1": 310, "x2": 800, "y2": 435}
+        self.blue_player_bbox: Dict[str, int] = {"x1": 680, "y1": 515, "x2": 750, "y2": 635}
+        self.white_player_bbox: Dict[str, int] = {"x1": 730, "y1": 320, "x2": 790, "y2": 415}
+        self.blue_player_img_path: str = "data/images/blue_player.jpg"
+        self.white_player_img_path: str = "data/images/white_player.jpg"
 
         self.image_reszie_size: Tuple[int, int] = (20, 45)
+
+        self.classifier_xml_file: str = "data/classifier.xml"
+
